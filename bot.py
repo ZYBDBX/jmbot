91bd07391b837837d7ffe4c483aa53afbcbe19d1
ZYBDBX <1340265938@qq.com>
2025-07-05 18:22:40 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,57 @@
+from ncatbot.core import BotClient
+from ncatbot.core import GroupMessage
+import os
+from wand.image import Image as WandImage
+from ncatbot.utils import config
+import jmcomic
+
+config.set_bot_uin("")  # 设置 bot qq 号 (必填)
+config.set_root("1340265938")  # 设置 bot 超级管理员账号 (建议填写)
+config.set_webui_uri("http://127.0.0.1:3011")  # 设置 webui 地址 (必填)
+config.set_webui_token("napcat")
+config.set_ws_uri("ws://127.0.0.1:3001")
+config.set_ws_token("")
+bot = BotClient()
+
+@bot.group_event()
+async def on_group_message(msg: GroupMessage):
+    if msg.raw_message.isdigit():  # 检测消息是否为纯数字
+        download_member = msg.raw_message
+        await download_and_convert(msg.group_id, download_member)
+
+async def download_and_convert(group_id, download_member):
+    current_dir = os.path.dirname(os.path.abspath(__file__))  # 获取当前文件夹路径
+    download_dir = os.path.join(current_dir, 'download')  # 设置下载文件夹为当前文件夹下的 download 文件夹
+
+    # 如果 download 文件夹不存在，则创建它
+    if not os.path.exists(download_dir):
+        os.makedirs(download_dir)
+
+    # 切换到 download_dir 目录下
+    os.chdir(download_dir)
+
+    # 使用 jmcomic 库下载文件
+    jmcomic.download_album(download_member)
+
+    # 切换回原来的目录
+    os.chdir(current_dir)
+
+    # 自动检测 download 文件夹中最新的文件夹
+    latest_folder = max([os.path.join(download_dir, d) for d in os.listdir(download_dir)], key=os.path.getmtime)
+
+    # 将最新的文件夹内的 webp 文件转换成 pdf
+    webp_files = [f for f in os.listdir(latest_folder) if f.endswith('.webp')]
+    pdf_path = os.path.join(latest_folder, f'{download_member}.pdf')
+
+    with WandImage() as pdf:
+        for webp_file in webp_files:
+            webp_path = os.path.join(latest_folder, webp_file)
+            with WandImage(filename=webp_path) as img:
+                img.format = 'pdf'
+                pdf.sequence.append(img)
+        pdf.save(filename=pdf_path)
+
+    # 将 pdf 发送到检测到这个数字的 QQ 群
+    await bot.api.post_group_file(group_id=group_id, file=pdf_path)
+          
+bot.run(enable_webui_interaction=False)
---
+++ readme.md
@@ -0,0 +1,20 @@
+step1 先运行压缩包内的python安装程序（如事先已经安装好请忽略此步）
+(1)双击之后勾选下方的"Add python.exe to PATH"之后选择Install Now
+(2)等待进度条跑完 step2 win+r
+输入cmd并把引号内的全部内容复制到运行框内\|（不要带引号并且请等待显示Successfully的字眼后再复制下一个）
+"pip install somepackage -i https://pypi.tuna.tsinghua.edu.cn/simple pip
+install jmcomic pip install ncatbot pip install wand" 在运行"pip install
+wand"后 双击运行ImageMagick-7.1.1-47-Q16-x64-dll.exe step3
+找到Napcat.Shell.Windows.Onekey.zip并解压
+双击运行其中的exe文件并等待进度条跑完
+跑完之后双击napcat.bat并根据提示扫码登录（注意：登陆后请保持该窗口的开启）
+step4 进入浏览器 输入127.0.0.1:3011/webui 密码（token）初始为napcat
+点击网络配置 点击新建http服务端并且保持一切默认 之后启用该项设置 step5
+双击运行bot.py 根据要求填入机器人QQ号 之后如果显示机器人xxxx成功启动
+则可以把机器人拉入群聊中 在群聊发送数字进行检测
+如在发出数字后自动跑代码则为成功 注意事项：
+（1）注意检测时请在群聊中发送纯数字 注意条件群聊与纯数字
+如"1234""1456"而类似"jm1234"或"经历1场战斗和2个敌人作战最终战胜3人"之类的则不会识别
+（2）请不要在短时间内反复打开napcat.bat 否则会有封号风险
+（3）如有一切本文没有提及的问题 欢迎各位提出 联系qq：1340265938
+在线时间：工作日21:00-23:00 节假日几乎任意时间
